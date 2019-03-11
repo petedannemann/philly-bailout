d2104e9492c15f9b1e7e002e335dc7492bea3e93
Peter Dannemann <peter.dannemann@phila.gov>
2019-03-11 11:12:12 -0400

    Add dj_database_url

--- bailout/settings/prod.py
+++ bailout/settings/prod.py
@@ -1,14 +1,26 @@
 """ Production Settings """
 
 import os
+
+import dj_database_url
+
 from .dev import *
 
+
+############
+# DATABASE #
+############
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}
 
 ############
 # SECURITY #
 ############
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 DEBUG = False
 # Set to your Domain here
